c48bf0b579cd9725c590599fd5db7da4d10f87ed
frank <frank220203@gmail.com>
2025-03-12 11:20:56 +0900

    Kafka 테스트 코드 작성

--- app/tests/api/controllers/test_fillings_controller.py
+++ app/tests/api/controllers/test_fillings_controller.py
@@ -17,7 +17,7 @@ async def test_get_documents_urls(
     mock_fillings_usecase.get_documents_urls = AsyncMock()
     mock_fillings_usecase.get_documents_urls.return_value = ["url1", "url2"]
     
-    # Mocking
+    # Mocking // 의존성 주입을 안 할 경우 실제 API를 타게 됨 (통합테스트 가능)
     app.dependency_overrides[get_fillings_usecase] = lambda: mock_fillings_usecase
 
     # When
@@ -27,15 +27,15 @@ async def test_get_documents_urls(
     assert response.status_code == 200
     assert response.json() == {"urls": ["url1", "url2"]}
 
-# get_portfolios 단위 테스트
+# get_portfolios_urls 단위 테스트
 @pytest.mark.anyio
-async def test_get_portfolios_urls(
+async def test_get_portfolios(
     async_client: AsyncClient,
     mock_fillings_usecase: MagicMock
     ) -> None:
     # Mock
-    mock_fillings_usecase.get_portfolios_urls = AsyncMock()
-    mock_fillings_usecase.get_portfolios_urls.return_value = ["url1", "url2"]
+    mock_fillings_usecase.get_portfolios = AsyncMock()
+    mock_fillings_usecase.get_portfolios.return_value = {'meta':'data', 'urls':['url1', 'url2']}
 
     # Mocking
     app.dependency_overrides[get_fillings_usecase] = lambda: mock_fillings_usecase
@@ -45,4 +45,24 @@ async def test_get_portfolios_urls(
 
     # Then
     assert response.status_code == 200
-    assert response.json() == {"porfolios_urls": ["url1", "url2"]}
+    assert response.json() == {"porfolios": {'meta':'data', 'urls':['url1', 'url2']}}
+
+# get_portfolios_issuers 단위 테스트
+@pytest.mark.anyio
+async def test_get_portfolio_issuers(
+    async_client: AsyncClient,
+    mock_fillings_usecase: MagicMock
+) -> None:
+    # Mock
+    mock_fillings_usecase.get_portfolio_issuers = AsyncMock()
+    mock_fillings_usecase.get_portfolio_issuers.return_value = ["stock1", "stock2"]
+
+    # Mocking
+    # app.dependency_overrides[get_fillings_usecase] = lambda: mock_fillings_usecase
+
+    # When
+    response = await async_client.get(f"{settings.API_V1_STR}/fillings/portfolio/issuers?email=sample@email.com&endpoint=/Archives/edgar/data/1067983/000095012324011775/xslForm13F_X02/36917.xml")
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == {"porfolio_issuers": ["stock1", "stock2"]}
--- app/tests/domain/usecases/test_fillings_usecase.py
+++ app/tests/domain/usecases/test_fillings_usecase.py
@@ -5,6 +5,7 @@ from domain.usecases.fillings_usecase import FillingsUsecase
 # get_documents_urls 단위 테스트
 @pytest.mark.anyio
 async def test_get_documents_urls(
+    mock_kafka_service: MagicMock,
     mock_parser_service: MagicMock, 
     mock_edgar_api_service: MagicMock
     ) -> None:
@@ -14,7 +15,7 @@ async def test_get_documents_urls(
     mock_edgar_api_service.get_fillings_list.return_value = "<html><body><table class='tableFile2'><tr><th>Description</th></tr><tr><td>13F-HR</td><td><a href='url1'> Documents</a></td></tr><tr><td>13F-HR</td><td><a href='url2'> Documents</a></td></tr></table></body></html>"
 
     # When & Mocking
-    fillings_usecase = FillingsUsecase(mock_parser_service, mock_edgar_api_service)
+    fillings_usecase = FillingsUsecase(mock_kafka_service, mock_parser_service, mock_edgar_api_service)
     urls = await fillings_usecase.get_documents_urls(email="sample@email.com", endpoint="/cgi-bin/browse-edgar/getcompany", cik="0001067983", type="13F-HR")
 
     # Then
@@ -22,15 +23,38 @@ async def test_get_documents_urls(
 
 # get_portfolios_urls 단위 테스트
 @pytest.mark.anyio
-async def test_get_portfolios_urls(
+async def test_get_portfolios(
+    mock_kafka_service: MagicMock,
     mock_parser_service: MagicMock, 
     mock_edgar_api_service: MagicMock   
 ) -> None:
     # Given & Mock
-    mock_parser_service.find_portfolios_urls.return_value = ['url1', 'url2']
+    mock_parser_service.find_portfolios.return_value = {'meta':'data', 'urls':['url1', 'url2']}
     mock_edgar_api_service.get_fillings_list = AsyncMock()
     mock_edgar_api_service.get_fillings_list.return_value = "<html><body><table class='tableFile'><tr><th>Description</th></tr><tr><td></td><td>13F-HR</td><td><a href='url1'> Documents</a></td></tr><tr><td></td><td>13F-HR</td><td><a href='url2'> Documents</a></td></tr></table></body></html>"
 
     # When & Mocking
-    fillings_usecase = FillingsUsecase(mock_parser_service, mock_edgar_api_service)
-    urls = await fillings_usecase.get_portfolios_urls(email="sample@email.com", endpoint="/Archives/edgar/data/1067983/000095012324011775/0000950123-24-011775-index.htm")
+    fillings_usecase = FillingsUsecase(mock_kafka_service, mock_parser_service, mock_edgar_api_service)
+    protfolios = await fillings_usecase.get_portfolios(email="sample@email.com", endpoint="/Archives/edgar/data/1067983/000095012324011775/0000950123-24-011775-index.htm")
+
+    # Then
+    assert protfolios == {'meta':'data', 'urls':['url1', 'url2']}
+
+# get_portfolio_issers 단위 테스트
+@pytest.mark.anyio
+async def test_get_portfolio_issuers(
+    mock_kafka_service: MagicMock,
+    mock_parser_service: MagicMock, 
+    mock_edgar_api_service: MagicMock   
+) -> None:
+    # Given & Mock
+    mock_parser_service.find_portfolio_issuers.return_value = ['stock1', 'stock2']
+    mock_edgar_api_service.get_fillings_list = AsyncMock()
+    mock_edgar_api_service.get_fillings_list.return_value = "<html><body><table class='tableFile'><tr><th>Description</th></tr><tr><td></td><td>13F-HR</td><td><a href='url1'> Documents</a></td></tr><tr><td></td><td>13F-HR</td><td><a href='url2'> Documents</a></td></tr></table></body></html>"
+
+    # When & Mocking
+    fillings_usecase = FillingsUsecase(mock_kafka_service, mock_parser_service, mock_edgar_api_service)
+    urls = await fillings_usecase.get_portfolio_issuers(email="sample@email.com", endpoint="/Archives/edgar/data/1067983/000095012324011775/xslForm13F_X02/36917.xml")
+
+    # Then
+    assert urls == ['stock1', 'stock2']
