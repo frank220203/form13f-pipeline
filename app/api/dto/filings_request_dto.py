87b5aac5b9618d8a23eeadf74645be5d558049b2
frank <frank220203@gmail.com>
2025-08-07 15:46:10 +0900

    [리팩토링] DTO endopoint 옵션화

--- app/api/dto/filings_request_dto.py
+++ app/api/dto/filings_request_dto.py
@@ -3,7 +3,7 @@ from pydantic import Field, BaseModel, model_validator
 
 class FilingsRequestDto(BaseModel):
     email: str = Field(..., description="SEC EDGAR의 정책으로 User-Agent로 회사 메일 요구")
-    endpoint: str = Field(..., description="ex) /cgi-bin/browse-edgar/getcompany")
+    endpoint: Optional[str] = None
     meta: Optional[dict] = None
     params: Optional[dict] = None
     headers: Optional[dict] = None
@@ -11,5 +11,4 @@ class FilingsRequestDto(BaseModel):
     @model_validator(mode='after')
     def make_headers(cls, values):
         values.headers = {'User-Agent': values.email}
-        print(values)
         return values
