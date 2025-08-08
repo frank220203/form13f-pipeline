47b2b3f08aad355c2b6ffebf6f40ed3e61c9b341
frank <frank220203@gmail.com>
2025-08-08 16:21:40 +0900

    [리팩토링] api 리턴 타입 string으로 원복

--- app/domain/usecases/services/api_caller.py
+++ app/domain/usecases/services/api_caller.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractmethod
 
 class ApiCaller(ABC):
     @abstractmethod
-    def call(self, url: str, headers: dict, params: dict) -> dict:
+    def call(self, url: str, headers: dict, params: dict) -> str:
         pass
