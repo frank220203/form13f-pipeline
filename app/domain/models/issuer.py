c702716ffb44e3fe643851f8b6d59617e151246c
frank <frank220203@gmail.com>
2025-08-26 10:23:30 +0900

    [Load] load_portfolio 테스트 완료

--- app/domain/models/issuer.py
+++ app/domain/models/issuer.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, AliasChoices
 
 class Issuer(BaseModel):
-    name_of_issuer: str = Field(..., alias='nameOfIssuer', description="상장회사 이름")
-    tilte_of_class: str = Field(..., alias='titleOfClass', description="주식 종류")
+    name_of_issuer: str = Field(..., alias=AliasChoices('nameOfIssuer', 'name_of_issuer'), description="상장회사 이름")
+    tilte_of_class: str = Field(..., alias=AliasChoices('titleOfClass', 'tilte_of_class'), description="주식 종류")
     cusip: str = Field(..., description="미국에서 사용하는 주식 고유 식별 번호/국내도 형식만 다를 뿐, 동일")
     value: str = Field(..., description="주식 가치")
-    shrs_or_prn_amt: dict = Field(..., alias='shrsOrPrnAmt', description="sshPrnamt : 주식 수량 / sshPrnamtType: SH(주식), PRN(채권)")
-    investment_discretion: str = Field(..., alias='investmentDiscretion', description="투자 재량권")
-    other_manager: str = Field(..., description="해당 주식 관리자")
-    voting_authority: dict = Field(..., description="투표권")
+    shrs_or_prn_amt: dict = Field(..., alias=AliasChoices('shrsOrPrnAmt', 'shrs_or_prn_amt'), description="sshPrnamt : 주식 수량 / sshPrnamtType: SH(주식), PRN(채권)")
+    investment_discretion: str = Field(..., alias=AliasChoices('investmentDiscretion', 'investment_discretion'), description="투자 재량권")
+    other_manager: str = Field(..., alias=AliasChoices('otherManager', 'other_manager'), description="해당 주식 관리자")
+    voting_authority: dict = Field(..., alias=AliasChoices('votingAuthority', 'voting_authority'), description="투표권")
--- app/domain/usecases/pipeline_usecase.py
+++ app/domain/usecases/pipeline_usecase.py
@@ -2,17 +2,23 @@ import json
 from typing import List
 
 from domain.models.ticker import Ticker
+from domain.models.issuer import Issuer
+from domain.models.portfolio import Portfolio
 from domain.models.submission import Submission
+
 from domain.usecases.repositories.ticker_repository import TickerRepository
+from domain.usecases.repositories.portfolio_repository import PortfolioRepository
 from domain.usecases.repositories.submission_repository import SubmissionRepository
 
 class PipelineUsecase:
 
     __ticker_repository: TickerRepository
+    __portfolio_repository: PortfolioRepository
     __submission_repository: SubmissionRepository
 
-    def __init__(self, ticker_repository: TickerRepository, submission_repository: SubmissionRepository):
+    def __init__(self, ticker_repository: TickerRepository, portfolio_repository: PortfolioRepository, submission_repository: SubmissionRepository):
         self.__ticker_repository = ticker_repository
+        self.__portfolio_repository = portfolio_repository
         self.__submission_repository = submission_repository
 
     async def load_tickers(self, data: str) -> List[dict]:
@@ -40,3 +46,20 @@ class PipelineUsecase:
             raise e     
         
         return await self.__submission_repository.add_data(Submission(**data))
+    
+    async def load_portfolios(self, data: str) -> dict:
+
+        try:
+            while isinstance(data, str):
+                data = json.loads(data)
+        except Exception as e:
+            raise e     
+        
+        portfolio_dict = {}
+        portfolio_dict.update(header_data=data['edgarSubmission']['headerData'], form_data=data['edgarSubmission']['formData'])
+        issuers = []
+        for issuer in data['informationTable']['infoTable'] :
+            issuers.append(Issuer(**issuer))
+        portfolio_dict.update({'issuers':issuers})
+        
+        return await self.__portfolio_repository.add_data(Portfolio(**portfolio_dict))
