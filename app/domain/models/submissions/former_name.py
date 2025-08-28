aae9b5d4b1be01ba1add4339583111ddcf056e52
frank <frank220203@gmail.com>
2025-08-28 16:07:24 +0900

    [리팩토링] Submission 데이터 객체화, 테스트 완료 파일 추가

--- app/domain/models/submissions/former_name.py
+++ app/domain/models/submissions/former_name.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, AliasChoices
 
 class FormerName(BaseModel):
     name: str
-    start: str = Field(..., alias='from')
+    start: str = Field(..., alias=AliasChoices('from', 'start'))
     to: str
